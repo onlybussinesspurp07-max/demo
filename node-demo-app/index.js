cff031cdb6c80cbf3d34a52fe6ead156a2ddbbdd
onlybussinesspurp07-max <onlybussinesspurp07@gmail.com>
2025-11-18 23:04:12 +0530

    Update index.js

--- node-demo-app/index.js
+++ node-demo-app/index.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// app.listen(3000, () => {
-//   console.log("Server running on http://localhost:3000");
-// });
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
