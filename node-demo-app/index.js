85a8e7756200990771043651f8cddc348ec541cb
onlybussinesspurp07-max <onlybussinesspurp07@gmail.com>
2025-11-18 23:18:15 +0530

    Update index.js

--- node-demo-app/index.js
+++ node-demo-app/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+// const express = require("express");
+// const path = require("path");
+// const app = express();
 
-// serve static files
-app.use(express.static(path.join(__dirname, "public")));
+// // serve static files
+// app.use(express.static(path.join(__dirname, "public")));
 
-// send index.html for the root
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// // send index.html for the root
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
-// fallback for all other routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// // fallback for all other routes
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
-module.exports = app;
+// module.exports = app;
